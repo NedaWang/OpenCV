b822d3c13f29e090f201c82c882835c2c7205009
NedaWang <65266814+NedaWang@users.noreply.github.com>
2021-10-09 22:31:06 +1100

    about pixel

--- image.py
+++ image.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 
+# 02_01
+
 #load in image
 img = cv2.imread("opencv-logo.png")
 
@@ -26,3 +28,18 @@ cv2.imwrite("output.jpg",img)
 
 # in terminal, type 'du -a' can see size of images
 # the size is different since OpenCV change the encoding of the image that is saved out to disk.
+
+
+
+
+# 02_02  pixel
+print(type(img)) # numpy.ndarray
+print(img.shape)
+print(len(img)) # row pixel number
+print(len(img[0])) # column pixel number
+print(len(img[0][0])) # channel number 3 indicates BGR, if there is transparency layer or an alpha layer that should be 4
+print(img.dtype) # unit8: unsigned integer value of 8, a maximum of 2 to 8 values in each pixel, which means the rangs is 0 to 255
+
+print(img[10,5])
+print(img[:,:,0]) # all the pixels in the first channel
+print(img.size)  # total number of pixels

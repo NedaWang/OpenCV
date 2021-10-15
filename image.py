cbea42efa10f503563d20c38200f1e9208624c3d
NedaWang <65266814+NedaWang@users.noreply.github.com>
2021-10-15 14:58:24 +1100

    simple drawing app

--- image.py
+++ image.py
@@ -264,3 +264,56 @@ while(True):   # forever loop
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+
+# 02_10 simple drawing app
+
+# Global variables
+canvas = np.ones([500,500,3],'uint8')*255
+
+red = (0,0,255)
+blue = (255,0,0)
+radius = 10
+line_width = -1
+point = (0, 0)
+color = red
+pressed = False
+
+# click callback
+def click(event, x, y, flags, param):
+	global canvas, point, color, pressed
+	if event == cv2.EVENT_LBUTTONDOWN:
+		pressed = True
+		cv2.circle(canvas, (x, y), 10, blue, -1)
+
+	# mousemove isn't triggered after event_down
+	elif event == cv2.EVENT_MOUSEMOVE and pressed == True:
+		cv2.circle(canvas, (x, y), 1, color, -1)
+
+	elif event == cv2.EVENT_LBUTTONUP:
+		pressed = False
+		cv2.circle(canvas, (x, y), 20, red, 1)
+
+# window initialization and callback assignment
+cv2.namedWindow("canvas")
+cv2.setMouseCallback("canvas", click)
+
+# Forever draw loop
+while True:
+
+	cv2.imshow("canvas",canvas)
+
+	# key capture every 1ms
+	ch = cv2.waitKey(1)
+	if ch & 0xFF == ord('q'):
+		break
+	elif ch & 0xFF == ord('b'):
+		color = (255,0,0)
+	elif ch & 0xFF == ord('g'):
+		color = (0,255,0)
+	elif ch & 0xFF == ord('r'):
+		color = (0,0,255)
+
+cv2.destroyAllWindows()

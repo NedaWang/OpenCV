4ecb0b150e77d66438672f812417b87bd93a2664
NedaWang <65266814+NedaWang@users.noreply.github.com>
2021-10-18 16:56:16 +1100

    area, perimeter, centroid

--- 2_object_detection.py
+++ 2_object_detection.py
@@ -114,3 +114,42 @@ cv2.imshow("Contours", img2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+
+# 03_07 area, perimeter, centroid
+img = cv2.imread('detect_blob.png',1)
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+cv2.imshow("Binary", thresh)
+
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+img2 = img.copy()
+index = -1
+thickness = 4
+color = (255, 0, 255)
+
+objects = np.zeros([img.shape[0], img.shape[1],3], 'uint8')
+for c in contours:
+	# [c]: a list of a single contour that the one we're using right now
+	# -1: draw all the contours, which is just going to be the on countour on the list
+	# -1: fill in the color
+	cv2.drawContours(objects, [c], -1, color, -1)
+
+	area = cv2.contourArea(c)
+	perimeter = cv2.arcLength(c, True) # true: closed contour
+
+	M = cv2.moments(c) # image moment
+	# calculate centroid centre
+	cx = int( M['m10']/M['m00'])
+	cy = int( M['m01']/M['m00'])
+	cv2.circle(objects, (cx,cy), 4, (0,0,255), -1)
+
+	print("Area: {}, perimeter: {}".format(area,perimeter))
+
+cv2.imshow("Contours",objects)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()

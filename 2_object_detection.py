76ec911e728754e758de6b0957226e1da7af6a64
NedaWang <65266814+NedaWang@users.noreply.github.com>
2021-10-18 16:07:45 +1100

    skin detection

--- 2_object_detection.py
+++ 2_object_detection.py
@@ -62,3 +62,30 @@ cv2.imshow("dilate", dilate)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+
+# 03_04 skin detection
+img = cv2.imread('faces.jpeg',1)
+img = cv2.resize(img, (500,500))
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+h = hsv[:,:,0]  # hue: circular parameter, red is indicated by low numbers such as zero, or very high number such as 255. Blue and green are in the middle, represented as the more gray values.
+s = hsv[:,:,1]  # saturation
+v = hsv[:,:,2]  # intensity value
+# display side by side
+hsv_split = np.concatenate((h,s,v), axis=1)
+cv2.imshow("hsv", hsv_split)
+
+ret, min_sat = cv2.threshold(s,40,255, cv2.THRESH_BINARY)
+cv2.imshow("Sat Filter", min_sat)
+
+ret, max_hue = cv2.threshold(h,15,255, cv2.THRESH_BINARY_INV)  # inverse of the normal order of the threshold, so make 0-15 to white
+cv2.imshow("Hue Filter", max_hue)
+
+final = cv2.bitwise_and(min_sat, max_hue)
+cv2.imshow("Final", final)
+cv2.imshow("Original", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()

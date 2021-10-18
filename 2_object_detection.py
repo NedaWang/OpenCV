86b08ffd6af6edf9a1bfeebc08e274761e5632c4
NedaWang <65266814+NedaWang@users.noreply.github.com>
2021-10-18 16:38:47 +1100

    contour

--- 2_object_detection.py
+++ 2_object_detection.py
@@ -89,3 +89,28 @@ cv2.imshow("Original", img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+# 03_06 contour
+# print(cv2.__version__)  # 4.0.1
+# contour is an iterative energy reduction algorithm,
+img = cv2.imread('detect_blob.png',1)
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,155,1)
+cv2.imshow("Binary", thresh)
+
+#contours: actually list of individual contours and recall that each contour is a list of points which describe a parameter
+#hierarchy: a parant-child relationship of all the contours. A child would be if one contour is enclosed by another.
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+img2 = img.copy()
+index = -1   # -1: draw all contours
+thickness = 4
+color = (255, 0 , 255)
+
+cv2.drawContours(img2, contours, index, color, thickness)
+cv2.imshow("Contours", img2)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()

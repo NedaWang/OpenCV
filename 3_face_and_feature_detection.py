4d40670c71e6ab80379decc2a8834c2b0aa02529
NedaWang <65266814+NedaWang@users.noreply.github.com>
2021-10-20 11:46:56 +1100

    face detection

--- 3_face_and_feature_detection.py
+++ 3_face_and_feature_detection.py
@@ -19,3 +19,26 @@ cv2.imshow("Matching",result)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+
+# 04_05 face detection
+img = cv2.imread("faces.jpeg",1)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+path = "haarcascade_frontalface_default.xml"
+
+face_cascade = cv2.CascadeClassifier(path)
+
+#scaleFactor: compensating factor for only wanting faces close to the camera
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.10, minNeighbors=5, minSize=(40,40))
+print(len(faces))
+
+# x, y within height variable
+for (x, y, w, h) in faces:
+	# (x, y): top left corner
+	# (x+w,y+h): bottom right corner
+	cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+cv2.imshow("Image",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
